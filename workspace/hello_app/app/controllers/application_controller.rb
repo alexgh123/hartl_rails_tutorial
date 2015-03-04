b50dc8211a7b03567f44342ece1147dcdab0b9c9
Alex Hardt <hardt.alex@gmail.com>
2015-03-04 12:15:58 -0600

    upside down exclamation mark

--- workspace/hello_app/app/controllers/application_controller.rb
+++ workspace/hello_app/app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "hola mundo"
+    render text: "\xA1" + "hola mundo"
   end
 end
